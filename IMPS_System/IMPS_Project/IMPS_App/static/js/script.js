15805fff7570af4e2b4dacb47cbc7cc09f9a5908
CrisTinaMoran <khirviecliffordbautista@gmail.com>
2024-12-04 02:34:44 +0800

    Updated Version

--- IMPS_System/IMPS_Project/IMPS_App/static/js/script.js
+++ IMPS_System/IMPS_Project/IMPS_App/static/js/script.js
@@ -7,3 +7,28 @@ window.addEventListener("blur", () => {
   window.addEventListener("focus", () => {
     document.title = "Welcome To IETI Marikina!"
   });
+
+  // Function to handle tab switching
+  function openFloor(evt, floorName) {
+    var i, tabcontent, tablinks;
+    
+    // Hide all images
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    // Remove active class from all buttons
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the selected floor image and add active class to the button
+    document.getElementById(floorName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    // Update the iframe with the selected floor image
+    var iframe = document.getElementById("floorImage");
+    iframe.src = "{% static 'Images/" + floorName + ".png' %}";
+  }
