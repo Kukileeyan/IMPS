0bad0f511c2a12bd90cd6a5dca68e05bc3991261
CrisTinaMoran <khirviecliffordbautista@gmail.com>
2024-12-04 01:37:25 +0800

    Updated Version

--- IMPS_System/IMPS_Project/IMPS_App/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImpsAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'IMPS_App'
--- IMPS_System/IMPS_Project/IMPS_App/static/js/script.js
+++
@@ -1,9 +0,0 @@
-console.log('JavaScript is connected!');
-
-window.addEventListener("blur", () => {
-    document.title = "Idle";
-  });
-
-  window.addEventListener("focus", () => {
-    document.title = "Welcome To IETI Marikina!"
-  });
