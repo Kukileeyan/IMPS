1d8cea8281948ebe5985f0c2be5e5ef6671b8493
CrisTinaMoran <khirviecliffordbautista@gmail.com>
2024-12-04 16:09:25 +0800

    Updated Version

--- IMPS_System/IMPS_Project/settings.py
+++ IMPS_System/IMPS_Project/settings.py
@@ -118,8 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "IMPS_App/static",
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
